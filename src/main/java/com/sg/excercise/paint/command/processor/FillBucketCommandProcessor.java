dfaf6050d7ef81262f684dd5ed519883919aede0
anilmypropertybook <anil@mypropertybook.co.uk>
2021-12-05 16:18:18 +0530

    Change Fillbucket logic to stack from recursion to avoid stackoverflow

--- src/main/java/com/sg/excercise/paint/command/processor/FillBucketCommandProcessor.java
+++ src/main/java/com/sg/excercise/paint/command/processor/FillBucketCommandProcessor.java
@@ -5,10 +5,12 @@ import com.sg.excercise.paint.exception.InvalidCommandException;
 import com.sg.excercise.paint.model.BaseEntity;
 import com.sg.excercise.paint.model.CanvasEntity;
 import com.sg.excercise.paint.model.FillBucketEntity;
+import com.sg.excercise.paint.model.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
+import java.util.Stack;
 
 @Component
 public class FillBucketCommandProcessor extends AbstractCommandProcessor {
@@ -30,24 +32,34 @@ public class FillBucketCommandProcessor extends AbstractCommandProcessor {
         int x1 = fillBucketEntity.getX1();
         int y1 = fillBucketEntity.getY1();
         if (isPointOutsideCanvas(x1, y1, canvasEntity.getWidth(), canvasEntity.getHeight())) {
-            throw new InvalidCommandException(MessageFormat.format(appProperties.getMessages().getException().getInvalidCommandPointOutsideCanvas(),canvasEntity.getWidth(),canvasEntity.getHeight()));
+            throw new InvalidCommandException(MessageFormat.format(appProperties.getMessages().getException().getInvalidCommandPointOutsideCanvas(), canvasEntity.getWidth(), canvasEntity.getHeight()));
         }
         char originalChar = canvasEntity.getCanvasDataArray()[y1 - 1][x1 - 1];
-        recursiveFillBucketWithChar(x1 - 1, y1 - 1, originalChar, fillBucketEntity.getFillByChar(), canvasEntity);
+        fillBucketWithChar(x1 - 1, y1 - 1, originalChar, fillBucketEntity.getFillByChar(), canvasEntity);
 
     }
 
-    private void recursiveFillBucketWithChar(int x1, int y1, char originalChar, char fillByChar, CanvasEntity canvasEntity) {
+
+    private void fillBucketWithChar(int x1, int y1, char originalChar, char fillByChar, CanvasEntity canvasEntity) {
+        Stack<Point> recursiveStack = new Stack<>();
         char[][] canvasDataArray = canvasEntity.getCanvasDataArray();
-
-        if ((x1 >= 0 && x1 < canvasEntity.getWidth()) && (y1 >= 0 && y1 < canvasEntity.getHeight())) {
-            if (canvasDataArray[y1][x1] == originalChar) {
-
-                canvasEntity.getCanvasDataArray()[y1][x1] = fillByChar;
-                recursiveFillBucketWithChar(x1 + 1, y1, originalChar, fillByChar, canvasEntity);
-                recursiveFillBucketWithChar(x1 - 1, y1, originalChar, fillByChar, canvasEntity);
-                recursiveFillBucketWithChar(x1, y1 + 1, originalChar, fillByChar, canvasEntity);
-                recursiveFillBucketWithChar(x1, y1 - 1, originalChar, fillByChar, canvasEntity);
+        recursiveStack.add(new Point(x1, y1));
+        while (!recursiveStack.isEmpty()) {
+            Point point = recursiveStack.pop();
+            if (canvasDataArray[point.getX1()][point.getY1()] == originalChar) {
+                canvasDataArray[point.getX1()][point.getY1()] = fillByChar;
+            }
+            if (point.getX1() - 1 >= 0 && canvasDataArray[point.getX1() - 1][point.getY1()] == originalChar) {
+                recursiveStack.add(new Point(point.getX1() - 1, point.getY1()));
+            }
+            if (point.getX1() + 1 < canvasEntity.getHeight() && canvasDataArray[point.getX1() + 1][point.getY1()] == originalChar) {
+                recursiveStack.add(new Point(point.getX1() + 1, point.getY1()));
+            }
+            if (point.getY1() - 1 >= 0 && canvasDataArray[point.getX1()][point.getY1() - 1] == originalChar) {
+                recursiveStack.add(new Point(point.getX1(), point.getY1() - 1));
+            }
+            if (point.getY1() + 1 < canvasEntity.getWidth() && canvasDataArray[point.getX1()][point.getY1() + 1] == originalChar) {
+                recursiveStack.add(new Point(point.getX1(), point.getY1() + 1));
             }
         }
     }
